01163e3cb66cebc4cecffd6dcc175c01c6082000
Harold Artienda <haroldartienda@gmail.com>
2025-06-16 18:25:08 +0800

    update main dart and initial router

---
+++ lib/router.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:send_money/utils/constants.dart';
+
+final GoRouter router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: loginRoute,
+      builder: (ctx, state) {
+        return Scaffold();
+      },
+    ),
+  ],
+);
